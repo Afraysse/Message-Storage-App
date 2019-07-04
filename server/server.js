218f5f4f1feb69ad75df416ac4eea2ba23adb019
Anne Fraysse <annefraysse@Annes-MacBook-Pro.local>
2019-07-04 10:38:50 +0100

    working on creating a basic react app

---
+++ server/server.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+var cors = require('cors');
+
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Data = require('./data');
+
+const API_PORT = 3001;
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+// MongoDB database
+const dbRoute = 'mongodb://<your-db-username-here>:<your-db-password-here>@ds249583.mlab.com:49583/fullstack_app';
+
+// connects our backend code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// only made for logger
+// bodyParser, parses the request body to be a readable JSON format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// BREAK THIS OUT INTO API
+
+// GET METHOD - fetches all available data in our database
+router.get('/getData', (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+// UPDATE METHOD - overwrites existing data in db
+router.put('/updateData', (req, res) => {
+  const { id, update } = req.body;
+  Data.findByIdAndUpdate(id, update, (err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// POST METHOD - adds data to db
+router.post('/createData', (req, res) => {
+  let data = new Data();
+
+  const { id, message } = req.body;
+
+  if ((!id && id !== 0) || !message) {
+    return res.json({
+      success: false,
+      error: 'INVALID INPUTS'
+    });
+  }
+  data.message = message;
+  data.id = id;
+  data.save((err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// DELETE METHOD - deletes data from the db
+router.delete('/deleteData', (req, res) => {
+  const { id } = req.body;
+  Data.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(err);
+    return res.json({ succes: true });
+  });
+});
